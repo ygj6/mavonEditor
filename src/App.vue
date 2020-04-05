168260307cc38085cccaa78b87d1198c86899b80
WelcomeLi <weikanglee.china@gmail.com>
2020-04-05 17:27:26 +0800

    v2.8.3

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
       <h2 class="item-header">
         {{d_words.default_setting}}
       </h2>
-      <mavon-editor ref="md" :subfield = "subfield" :toolbarsFlag = "toolbarsFlag"  :codeStyle="code_style" :language = "d_language" :ishljs="true" @change="change" @save="saveone" class="item-editor" v-model="help1"
+      <mavon-editor ref="md" :subfield = "subfield" :toolbarsFlag = "toolbarsFlag" :toolbars="toolbars_large" :codeStyle="code_style" :language = "d_language" :ishljs="true" @change="change" @save="saveone" class="item-editor" v-model="help1"
       :external_link="external_link"></mavon-editor>
     </div>
     <div v-if="screen_phone" class="item">
@@ -60,6 +60,36 @@
         toolbarsFlag: true,
         default_open: 'edit',
         screen_phone: true,
+        toolbars_large: {
+          bold: true, // 粗体
+          italic: true, // 斜体
+          header: true, // 标题
+          underline: true, // 下划线
+          strikethrough: true, // 中划线
+          mark: true, // 标记
+          superscript: true, // 上角标
+          subscript: true, // 下角标
+          quote: true, // 引用
+          ol: true, // 有序列表
+          ul: true, // 无序列表
+          link: true, // 链接
+          imagelink: true, // 图片链接
+          code: true, // code
+          table: true, // 表格
+          fullscreen: true, // 全屏编辑
+          readmodel: true, // 沉浸式阅读
+          htmlcode: true, // 展示html源码
+          help: true, // 帮助
+          /* 1.3.5 */
+          undo: true, // 上一步
+          redo: true, // 下一步
+          trash: true, // 清空
+          /* 1.4.2 */
+          navigation: true, // 导航目录
+          /* 2.2.1 */
+          subfield: true, // 单双栏模式
+          preview: true, // 预览
+        },
         toolbars: {
           underline: true, // 下划线
           strikethrough: true, // 中划线
